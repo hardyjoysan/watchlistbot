e6aea50f4b98de208df1c74847399d4ba092475e
Hardy Joyson <hardyjoysan@gmail.com>
2020-09-20 17:09:49 +0530

    Register channel info

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ bot.command('start', (ctx) => {
   ctx.reply('<b><u>Hola Amigo! 😈</u></b><a href="https://t.me/joinchat/AAAAAFXx-J6srSdMQK9cgg"> Click here to join THE WATCHLIST to use this bot features</a>', { parse_mode:"HTML" });
 });
 
+bot.command('register', async (ctx) => {
+  let message = ctx.update.message;
+  let offset = message.entities[0].length;
+  let username = message.text.slice(offset).trim();
+
+  await bot.telegram.getChat(username)
+  .then(res => {
+    console.log(res);
+  })
+  .catch(error => {
+    ctx.reply('Invalid user name provided!')
+  })
+});
+
 bot.on('document', async (ctx) => {
     let document = ctx.update.message.document;
     await bot.telegram.sendDocument('1013218063', document.file_id, {caption: 'Join @The_Watchlist'});
