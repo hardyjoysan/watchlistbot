e27422af4d54ee5b6f6f97d7b2dc0f40cf12b13e
Hardy Joyson <hardyjoysan@gmail.com>
2020-09-21 01:19:27 +0530

    Fixed error while db connection

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import { MongoClient } from 'mongodb';
 config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-const client = new MongoClient(process.env.MONGODB_CONNECT, { useUnifiedTopology: true });
 
 bot.command('start', (ctx) => {
   ctx.reply('<b><u>Hola Amigo! 😈</u></b><a href="https://t.me/joinchat/AAAAAFXx-J6srSdMQK9cgg"> Click here to join THE WATCHLIST to use this bot features</a>', { parse_mode:"HTML" });
@@ -19,30 +18,36 @@ bot.command('register', async (ctx) => {
   var doc = {user_id : message.from.id};
 
   await bot.telegram.getChat(username)
-  .then(res => {
-    doc = { ...doc, channel_id: res.id, channel_title: res.title}
-  })
-  .catch(() => {
-    ctx.reply('Invalid user name provided!')
-  });
+    .then(res => {
+      doc = { ...doc, channel_id: res.id, channel_title: res.title}
+    })
+    .catch(() => {
+      ctx.reply('Invalid user name provided!');
+    });
 
+  if (!doc.channel_id) return false;
+
+  const client = new MongoClient(process.env.MONGODB_CONNECT, { useUnifiedTopology: true });
+  
   (async function (){
-    try {
-      
-      await client.connect();
-      const database = client.db('watchlistbot');
-      const collection = database.collection('channels');
-      await collection.insertOne(doc)
-      .then(() => {
-        ctx.reply('Channel successfully registered.')
-      })
-      .catch(() => {
-        ctx.reply('Technical Error! Try again later.')
-      });
-      
-    } finally {
-      await client.close();
-    }
+      try {
+        await client.connect();
+        const database = client.db('watchlistbot');
+        const collection = database.collection('channels');
+        let check = await collection.findOne({user_id: doc.user_id, channel_id: doc.channel_id});
+        if (!check) {
+          await collection.insertOne(doc)
+            .then(() => {
+              return ctx.reply('Channel successfully registered.');
+            });
+        }else{
+          return ctx.reply('Submitted username already registerd with your account.');
+        }
+      } catch {
+        return ctx.reply('Technical Error! Try again later.');
+      } finally {
+        await client.close();
+      }
   }());
 
 });
